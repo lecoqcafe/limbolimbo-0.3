f935673db9509f43476f14a4e07d44020299ed53
LeCOQ.Café <info.lecoq.cafe@gmail.com>
2025-10-20 09:16:48 -0400

    Update App.tsx v.0.2.101

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Recherche from "./pages/Recherche";
 import OpportunitesPersonnalisees from "./pages/OpportunitesPersonnalisees";
 import Opportunites from "./pages/Opportunites";
 import OpportunityDetail from "./pages/OpportunityDetail";
+import APropos from "./pages/APropos";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -24,6 +26,7 @@ const App = () => (
           <Route path="/opportunites-personnalisees" element={<OpportunitesPersonnalisees />} />
           <Route path="/opportunites" element={<Opportunites />} />
           <Route path="/opportunite" element={<OpportunityDetail />} />
+          <Route path="/a-propos" element={<APropos />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
