4e55f792e5f1cfc2e54d4a3534deae2481975601
LeCOQ.Café <info.lecoq.cafe@gmail.com>
2025-11-07 23:41:49 -0500

    Update App.tsx v.0.3.104

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { Header } from "@/components/Header";
+import TermsGuard from "@/components/TermsGuard";
+import ConditionsPage from "@/pages/Conditions";
+
 import Home from "./pages/Home";
 import Recherche from "./pages/Recherche";
 import OpportunitesPersonnalisees from "./pages/OpportunitesPersonnalisees";
@@ -11,7 +15,6 @@ import Opportunites from "./pages/Opportunites";
 import OpportunityDetail from "./pages/OpportunityDetail";
 import APropos from "./pages/APropos";
 import NotFound from "./pages/NotFound";
-
 import ToutesLesOpportunites from "./pages/ToutesLesOpportunites";
 import Changelog from "./pages/Changelog";
 
@@ -23,18 +26,26 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recherche" element={<Recherche />} />
-          <Route path="/opportunites-personnalisees" element={<OpportunitesPersonnalisees />} />
-          <Route path="/opportunites" element={<Opportunites />} />
-          <Route path="/opportunites/toutes" element={<ToutesLesOpportunites />} />
-          <Route path="/opportunite" element={<OpportunityDetail />} />
-          <Route path="/changelog" element={<Changelog />} />
-          <Route path="/a-propos" element={<APropos />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Header />
+        <TermsGuard>
+          <Routes>
+            {/* Route publique pour lecture des Conditions */}
+            <Route path="/conditions" element={<ConditionsPage />} />
+
+            {/* Routes existantes de l’application */}
+            <Route path="/" element={<Home />} />
+            <Route path="/recherche" element={<Recherche />} />
+            <Route path="/opportunites-personnalisees" element={<OpportunitesPersonnalisees />} />
+            <Route path="/opportunites" element={<Opportunites />} />
+            <Route path="/opportunites/toutes" element={<ToutesLesOpportunites />} />
+            <Route path="/opportunite" element={<OpportunityDetail />} />
+            <Route path="/changelog" element={<Changelog />} />
+            <Route path="/a-propos" element={<APropos />} />
+
+            {/* CATCH-ALL */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </TermsGuard>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
